4232ab7ea7de8f4b5c37d4a7ce708608a12255c3
Aneesh G Upadhya <142137512+paradox-blaze@users.noreply.github.com>
2023-11-12 19:17:47 +0530

    changed the type of newsgrid to international for International

--- src/routes/International.jsx
+++ src/routes/International.jsx
@@ -4,7 +4,7 @@ import NewsGrid from '../components/NewsGrid';
 const International = () => {
     return (
         <>
-            <NewsGrid type={'russia'} />
+            <NewsGrid type={'international'} />
         </>
     )
 }
