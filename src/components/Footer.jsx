71ee31917a5e61ec34f7de1c9f4b45cb2bcfb5d6
Aneesh G Upadhya <142137512+paradox-blaze@users.noreply.github.com>
2023-11-23 08:33:41 +0530

    changed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,6 @@ const Footer = () => {
   return (
     <footer className="footer-container border-t border-gray-600 shadow-xl backdrop-blur-lg bg-white dark:bg-black text-black dark:text-white">
       <p className="copyright">&copy; Precise News {currentYear}</p>
-      <p className="footer-item">Aditya HR PES1UG22AM013</p>
-      <p className="footer-item">Aneesh Upadhya PES1UG22AM022</p>
-      <p className="footer-item">Adit Acharya PES1UG22AM009</p>
     </footer>
   );
 };
